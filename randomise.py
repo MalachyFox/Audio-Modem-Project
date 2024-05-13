5c3828f19bf0cb2e72af2332c8b8a61ec5aafc05
Edward Townsend <ejpt2@cam.ac.uk>
2024-05-13 16:48:07 +0100

    MODIFY: changed to noise smaller in mag. and not added to original signal

--- randomise.py
+++ randomise.py
@@ -1,18 +1,16 @@
 import csv
 import random
 
-file_name = 'weekend-challenge/file1.csv'
-
-with open(file_name, newline='') as infile:
-    reader = csv.reader(infile)
-    with open('output.csv', 'w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        for i in range(50):
-            new_row = [random.uniform(-1, 1)]
-            writer.writerow(new_row)
-        for row in reader:
-            modified_row = [float(value) + random.uniform(-1, 1) for value in row]
-            writer.writerow(modified_row)
-        for i in range(50):
-            new_row = [random.uniform(-1, 1)]
-            writer.writerow(new_row)
+def add_noise_to_csv(file_name):
+    with open(file_name, newline='') as infile:
+        reader = csv.reader(infile)
+        with open('output.csv', 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            for i in range(50):
+                new_row = [random.uniform(-0.05, 0.05)]
+                writer.writerow(new_row)
+            for row in reader:
+                writer.writerow(row)
+            for i in range(50):
+                new_row = [random.uniform(-0.05, 0.05)]
+                writer.writerow(new_row)
