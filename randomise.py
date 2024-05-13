30409348e50327b2396abe0a0c85eb80e3f87265
Edward Townsend <ejpt2@cam.ac.uk>
2024-05-13 16:43:09 +0100

    MODIFY: updated number of noisy samples added to start and end of signal

--- randomise.py
+++ randomise.py
@@ -7,12 +7,12 @@ with open(file_name, newline='') as infile:
     reader = csv.reader(infile)
     with open('output.csv', 'w', newline='') as outfile:
         writer = csv.writer(outfile)
-        for i in range(5):
+        for i in range(50):
             new_row = [random.uniform(-1, 1)]
             writer.writerow(new_row)
         for row in reader:
             modified_row = [float(value) + random.uniform(-1, 1) for value in row]
             writer.writerow(modified_row)
-        for i in range(5):
+        for i in range(50):
             new_row = [random.uniform(-1, 1)]
             writer.writerow(new_row)
