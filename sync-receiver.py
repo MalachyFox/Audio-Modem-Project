c51a534325ab266c033fd803339059375c564e2e
Taitan Li <tl545@cam.ac.uk>
2024-05-14 19:47:46 +0100

    receiver

--- sync-receiver.py
+++ sync-receiver.py
@@ -7,7 +7,7 @@ find_library('portaudio')
                                                    
 
 import matplotlib.pyplot as plt
-seconds = 3
+seconds = 12
 fs = 44100
 
 input = input('press space')
@@ -22,4 +22,4 @@ print(fftr)
 plt.plot(recording)
 plt.show()
 #plt.plot(np.absolute(fftr))
-visualize.plot_fft(fftr)
+visualize.plot_fft(fftr, fs)
