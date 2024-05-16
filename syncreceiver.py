17a9827c2baab205a4dfda177249b26a9749634e
Taitan Li <tl545@cam.ac.uk>
2024-05-16 18:09:33 +0100

    ooo

--- syncreceiver.py
+++ syncreceiver.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 seconds = 4
 fs = 44100
 
-f0 = 1000
+f0 = 0
 f1 = 2000
 
 sync = playsound.gen_chirp(f0,f1,fs,1)
@@ -42,21 +42,28 @@ print(len(sync))
 plt.plot(recording)
 plt.show()
 chirp = recording[position + len(sync)//2 :position+len(sync)]
+plt.plot(chirp)
+plt.show()
 fftr = np.fft.fft(chirp)
 sync1 = sync[len(sync)//2:]
+plt.plot(sync1)
+plt.show()
 ffts = np.fft.fft(sync1)
 
 #data = recording[position+block_length:position+block_length*2]
 
 plt.plot(correlation)
 plt.show()
-visualize.plot_fft(ffts, fs,f0,f1)
-visualize.plot_fft(fftr, fs,f0,f1)
+
+vf0 = 0
+vf1 = 2200
+visualize.plot_fft(ffts, fs,vf0,vf1)
+visualize.plot_fft(fftr, fs,vf0,vf1)
 
 print(len(fftr),len(ffts))
 
 channel = fftr / ffts#fftr[f0:f1] / ffts[f0:f1]
-visualize.plot_fft(channel, fs,f0,f1)
+visualize.plot_fft(channel, fs,vf0,vf1)
 visualize.plot_constellation(channel)
 
 impulse = np.fft.ifft(fftr/ffts)
